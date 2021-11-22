a53da78623197add1038d5e2ac91f4b9aad131eb
AnthonyKon <31120910+AnthonyKon@users.noreply.github.com>
2021-11-23 10:26:55 +1030

    first commit

---
+++ cmake-build-debug/CMakeFiles/rolodex.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/CP2/rolodex/Rolodex.cpp" "C:/CP2/rolodex/cmake-build-debug/CMakeFiles/rolodex.dir/Rolodex.cpp.obj"
+  "C:/CP2/rolodex/main.cpp" "C:/CP2/rolodex/cmake-build-debug/CMakeFiles/rolodex.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/rolodex.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../librolodex.dll.a"
+  "../rolodex.exe"
+  "../rolodex.exe.manifest"
+  "../rolodex.pdb"
+  "CMakeFiles/rolodex.dir/Rolodex.cpp.obj"
+  "CMakeFiles/rolodex.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rolodex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
